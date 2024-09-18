30cefd1e5f2b54035f661086819a085a39ae7cee
unknown <mochama.reza94@ui.ac.id>
2024-09-18 12:23:16 +0700

    bacacsv.py diupdate

--- bacacsv.py
+++ bacacsv.py
@@ -4,6 +4,15 @@ import csv
 def read_csv_file(file_name):
     data = []
 
+    # Membuka dan membaca file csv dengan with agar tertutup secara otomatis
+    with open(file_name, mode="r") as file:
+        temp = csv.reader(file)
+
+        # Iterasi setiap baris dan menyimpannya ke dalam list data
+        for row in temp:
+            data.append(row)
+            print(row)
+
     return data
 
 
